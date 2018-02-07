2a0e8bf92b874f764ea81883290e0013ff2cd9ca
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2018-02-07 16:44:52 +0100

    modified main file

--- main.cpp
+++ main.cpp
@@ -11,42 +11,24 @@ int main()
     Students student1;
     student1.index=1;
     student1.name="Krzysztof";
+    student1.surname = "Kowalczyk";
+    student1.address = "Wroclaw";
+    student1.pesel=644545343;
+    student1.sex="man";
 
     Employess employess1;
-    employess1.name = "Zygmunt";
+    employess1.salary = 4000,00;
+    employess1.name = "Jan";
+    employess1.surname = "Nowak";
+    employess1.address = "Warszawa";
     employess1.pesel = 456213545;
+    employess1.sex="man";
 
     users.add(student1);
     users.add(employess1);
 
     users.showAllUsers();
-    /*
-    Students students;
-    Workers workers;
 
-    //Student student2={7455,"Jan","Nowak"};
-    //students.addStudent(student2);
-    
-    Students student3={123456, 96040812345, "Jan", "Natalis", "M", "ul. Powstańców Śląskich 23/5, Wrocław"};
-    students.addStudent(student3);
-    
-    Workers worker1={72112354321, "Michał", "Górniak", "M", "ul. Grunwaldzka 98/2, Wrocław", 1200};
-    workers.addWorkers(worker1);
-    
-    //Student student1={1454,"Krzysztof","Kowalczyk"};
-    //students.addStudent(student1);
-
-    students.ShowAllStudents();
-    workers.ShowAllWorkers();
-    //students.SortStudentsIndex();
-    //students.ShowAllStudents();
-
-    //students.removeStudent(7455);
-    //students.removeStudent(48623);
-
-    //students.ShowAllStudents();
-
-    */
     return 0;
 }
 
